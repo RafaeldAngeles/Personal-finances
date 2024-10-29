6d9b201c492504aa3fc37c2dbf2924ff744af201
Rafael <rafael.angeles.toledano@gmail.com>
2024-10-29 16:00:09 -0300

    mudança na estrutura do projeto

--- back-end/src/modules/finance/finance.controller.ts
+++ back-end/src/modules/finance/finance.controller.ts
@@ -7,28 +7,4 @@ import { UpdateFinanceDto } from './dto/update-finance.dto';
 export class FinanceController {
   constructor(private readonly financeService: FinanceService) {}
 
-  @Post()
-  create(@Body() createFinanceDto: CreateFinanceDto) {
-    return this.financeService.create(createFinanceDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.financeService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.financeService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFinanceDto: UpdateFinanceDto) {
-    return this.financeService.update(+id, updateFinanceDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.financeService.remove(+id);
-  }
 }
